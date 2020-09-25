76eeba698925099bac96890082ee02a372a51333
vandan <vandangorade2000@gmail.com>
2020-09-25 22:00:00 +0530

    try3: branch PR

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
-using Test
 
+using Test
     @testset "MyExample.jl" begin
         # Write your tests here.
         @test my_f(2,1) == 5
         @test my_f(2,4) == 8
         @test my_f(3,1) == 7
-
+        @test my_f(1,1) == 8
 end
