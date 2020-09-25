8166a15cdf5c7bacf05585e403c5a977b65d8143
vandan <vandangorade2000@gmail.com>
2020-09-25 21:30:26 +0530

    try2: update testcase

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,6 @@
 module MyExample
 
-# Write your package code here.
+    include("extra_file.jl")
 
+    export my_f
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
-using MyExample
 using Test
 
-@testset "MyExample.jl" begin
-
+    @testset "MyExample.jl" begin
+        # Write your tests here.
+        @test my_f(2,1) == 5
+        @test my_f(2,4) == 8
+        @test my_f(3,1) == 7
 
 end
